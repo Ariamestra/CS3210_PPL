49a9c104b9e19423883f743659c37eb493a37368
Maria Estrada <119823752+Ariamestra@users.noreply.github.com>
2024-09-16 16:55:56 +0000

    Working Project 1

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
-import Project1Ex #[ Use the filename myProgram here]
+# Maria Estrada 
+#Programming Project 1 - Due Sep 22
+# Sep 19, 2024 - 10:24 -10:55
+#-------------------------------------------
+# Main.py
+import Project1Ex 
 
 while True:
     text = input('M@E > ') #Give your own prompt name here
@@ -6,3 +11,4 @@ while True:
     result = Project1Ex.run(text)  
     print(result)
 #------------------------------------------------------
+# Python Main.py
--- Project1Ex.py
+++ Project1Ex.py
@@ -1,37 +1,67 @@
+# Maria Estrada 
+#Programming Project 1 - Due Sep 22
+# Sep 19, 2024 - 10:24
+#-------------------------------------------
+#Project1Ex.py 
+
 DIGITS = '0123456789'
 
+# Position class to track current position in the input
+class Position:
+    def __init__(self, index, line_num, column_num, fn, text):
+        self.index = index
+        self.line_num = line_num
+        self.column_num = column_num
+        self.fn = fn
+        self.text = text
+
+    def advance(self, current_char=None):
+        self.index += 1
+        self.column_num += 1
+        if current_char == '\n':
+            self.line_num += 1
+            self.column_num = 0
+        return self
+
+    def copy(self):
+        return Position(self.index, self.line_num, self.column_num, self.fn, self.text)
+
+    
 class Token:
     def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
     def __repr__(self):
-        if self.value:
+        if self.value is not None:
             return f"{self.type}:{self.value}"
         return f"{self.type}"
 
+
 # Token Types
-ME_INT  = 'ME_INT'
+ME_INT = 'ME_INT'
 ME_FLOAT = 'ME_FLOAT'
-ME_PLUS  = 'ME_PLUS'
+ME_PLUS = 'ME_PLUS'
 ME_MINUS = 'ME_MINUS'
 ME_ILLEGAL = 'ME_ILLEGAL'
+ME_MULTIPLY = 'ME_MULTIPLY'
+ME_DIVIDE = 'ME_DIVIDE'
+ME_LPAREN = 'ME_LPAREN'
+ME_RPAREN = 'ME_RPAREN'
 
-class Lexer: # Converts input to tokens
-    def __init__(self, text):
+# Lexer class that tokenizes input
+class Lexer:
+    def __init__(self, text, fn="<stdin>"):
         self.text = text
-        self.pos = -1 # Starting Position 
+        self.pos = Position(-1, 1, 0, fn, text)  # Initialize with a Position object
         self.current_char = None
         self.advance()
 
-    def advance(self): # Go to next character
-        self.pos += 1
-        if self.pos < len(self.text):
-            self.current_char = self.text[self.pos]
-        else:
-            self.current_char = None
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
 
-    def make_tokens(self): # Make tokens from input
+    def make_tokens(self):
         tokens = []
         while self.current_char is not None:
             if self.current_char in ' \t':
@@ -44,21 +74,35 @@ class Lexer: # Converts input to tokens
             elif self.current_char == '-':
                 tokens.append(Token(ME_MINUS))
                 self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(ME_MULTIPLY))
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(ME_DIVIDE))
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(ME_LPAREN))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(ME_RPAREN))
+                self.advance()
             else:
                 tokens.append(self.make_illegal())
         return tokens
 
-    def make_digit(self): # Return tockens
+    def make_digit(self):
         num_str = ''
         while self.current_char is not None and self.current_char in DIGITS:
             num_str += self.current_char
             self.advance()
         return Token(ME_INT, int(num_str))
 
-    def make_illegal(self): # Handle illegal characters
+    def make_illegal(self):
+        pos_copy = self.pos.copy()
         illegal_char = self.current_char
-        self.advance()  # Move past the illegal character
+        self.advance()
         print(f"Illegal Character: '{illegal_char}'")
+        print(f"File {self.pos.fn}, line {pos_copy.line_num}, column {pos_copy.column_num}")
         return Token(ME_ILLEGAL, illegal_char)
 
 def run(text):
@@ -66,10 +110,15 @@ def run(text):
     tokens = lexer.make_tokens()
     return tokens
 
-# Test Cases
+# Test cases
 print(run("1 + 2"))
-print(run("1 - 2"))
+print(run("2 - 1"))
+print(run("4 * 5"))
+print(run("8 / 4"))
+print(run("(1 + 2) * 3"))
 print(run("d + 1"))
+
+
 
 
 
