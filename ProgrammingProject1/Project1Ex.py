f9a40231a9bfcc461323ac6bbf01ce52ffa210d7
Maria Estrada <119823752+Ariamestra@users.noreply.github.com>
2024-10-09 21:46:24 +0000

    Working on Lab 5

--- ProgrammingProject1/Project1Ex.py
+++ ProgrammingProject1/Project1Ex.py
@@ -5,7 +5,6 @@
 
 DIGITS = '0123456789'
 
-# Position class to track current position in the input
 class Position:
     def __init__(self, index, line_num, column_num, fn, text):
         self.index = index
@@ -25,7 +24,6 @@ class Position:
     def copy(self):
         return Position(self.index, self.line_num, self.column_num, self.fn, self.text)
 
-    
 class Token:
     def __init__(self, type_, value=None):
         self.type = type_
@@ -33,9 +31,8 @@ class Token:
 
     def __repr__(self):
         if self.value is not None:
-            return f"{self.type}:{self.value}"
-        return f"{self.type}"
-
+            return f"RR_{self.type}:{self.value}"
+        return f"RR_{self.type}"
 
 # Token Types
 ME_INT = 'ME_INT'
@@ -48,44 +45,48 @@ ME_DIVIDE = 'ME_DIVIDE'
 ME_LPAREN = 'ME_LPAREN'
 ME_RPAREN = 'ME_RPAREN'
 
-# NumberNode class to represent numbers in the abstract syntax tree (AST)
-class NumberNode:
+class ASTNode:
+    pass
+
+class BinOp(ASTNode):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def __repr__(self):
+        return f'({self.left}, {self.op}, {self.right})'
+
+class Num(ASTNode):
     def __init__(self, token):
-        self.token = token
+        self.value = token.value
 
     def __repr__(self):
-        return f"{self.token.value}"
+        return f'ME_INT:{self.value}'
 
-# OperationNode class to represent operations in the AST
-class OperationNode:
-    def __init__(self, left_node, op_token, right_node):
-        self.left_node = left_node
-        self.op_token = op_token
-        self.right_node = right_node
-
-    def __repr__(self):
-        return f"({self.left_node} {self.op_token.type} {self.right_node})"
-
-# Lexer class that tokenizes input
-class Lexer:
+# Parser class that tokenizes input
+class Parser:
     def __init__(self, text, fn="<stdin>"):
         self.text = text
-        self.pos = Position(-1, 1, 0, fn, text)  # Initialize with a Position object
+        self.pos = Position(-1, 1, 0, fn, text)
         self.current_char = None
+        self.tokens = self.make_tokens()
+        self.current_token = None
+        self.token_idx = -1
         self.advance()
 
     def advance(self):
-        self.pos.advance(self.current_char)
-        self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
+        self.token_idx += 1
+        if self.token_idx < len(self.tokens):
+            self.current_token = self.tokens[self.token_idx]
+        return self.current_token
 
-    def factor(self):
-        token = self.current_token
-        if token.type in (ME_INT, ME_FLOAT):
-            self.advance() 
-        return NumberNode(token)
+    def parse(self):
+        return self.expression()
 
     def make_tokens(self):
         tokens = []
+        self.advance_char()
         while self.current_char is not None:
             if self.current_char in ' \t':
                 self.advance()  # Skip spaces and tabs
@@ -111,49 +112,51 @@ class Lexer:
                 self.advance()
             else:
                 tokens.append(self.make_illegal())
+                self.advance_char()
         return tokens
+
+    def advance_char(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
 
     def make_digit(self):
         num_str = ''
         while self.current_char is not None and self.current_char in DIGITS:
             num_str += self.current_char
-            self.advance()
+            self.advance_char()
         return Token(ME_INT, int(num_str))
 
-    def make_illegal(self):
-        pos_copy = self.pos.copy()
-        illegal_char = self.current_char
-        self.advance()
-        print(f"Illegal Character: '{illegal_char}'")
-        print(f"File {self.pos.fn}, line {pos_copy.line_num}, column {pos_copy.column_num}")
-        return Token(ME_ILLEGAL, illegal_char)
-    
+    def factor(self):
+        token = self.current_token
+        if token.type == ME_INT:
+            self.advance()
+            return Num(token)
+        else:
+            raise Exception(f"Expected number, but got {token.type}")
+
     def term(self):
         left = self.factor()
-        opTree = left
-        while self.current_token is not None and self.current_token.type in (ME_MULTIPLY, ME_DIVIDE):
+        while self.current_token.type in (ME_MULTIPLY, ME_DIVIDE):
             op_token = self.current_token
-            self.advance()  # move to the next token
+            self.advance()
             right = self.factor()
-            opTree = OperationNode(left, op_token, right)
-            left = opTree  # Update left for the next loop iteration
-        return opTree
+            left = BinOp(left, op_token, right)
+        return left
 
     def expression(self):
         left = self.term()
-        opTree = left
-        while self.current_token is not None and self.current_token.type in (ME_PLUS, ME_MINUS):
+        while self.current_token.type in (ME_PLUS, ME_MINUS):
             op_token = self.current_token
-            self.advance()  # move to the next token
+            self.advance()
             right = self.term()
-            opTree = OperationNode(left, op_token, right)
-            left = opTree  # Update left for the next loop iteration
-        return opTree
+            left = BinOp(left, op_token, right)
+        return left
 
 def run(text):
-    lexer = Lexer(text)
-    tokens = lexer.make_tokens()
-    return tokens
+    parser = Parser(text)
+    AST = parser.parse()
+    return AST
+
 
 
 
