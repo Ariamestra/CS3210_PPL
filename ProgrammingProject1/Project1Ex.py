859078ba9b8ce3da69043d689df7844a2c9b14c4
Maria Estrada <119823752+Ariamestra@users.noreply.github.com>
2024-10-10 15:43:44 +0000

    Lab5

--- ProgrammingProject1/Project1Ex.py
+++ ProgrammingProject1/Project1Ex.py
@@ -5,6 +5,7 @@
 
 DIGITS = '0123456789'
 
+# Class to track the current position
 class Position:
     def __init__(self, index, line_num, column_num, fn, text):
         self.index = index
@@ -24,6 +25,7 @@ class Position:
     def copy(self):
         return Position(self.index, self.line_num, self.column_num, self.fn, self.text)
 
+# Token class to represent the type and value of tokens
 class Token:
     def __init__(self, type_, value=None):
         self.type = type_
@@ -45,9 +47,11 @@ ME_DIVIDE = 'ME_DIVIDE'
 ME_LPAREN = 'ME_LPAREN'
 ME_RPAREN = 'ME_RPAREN'
 
+# Base class for nodes in AST
 class ASTNode:
     pass
 
+# Binary operations
 class BinOp(ASTNode):
     def __init__(self, left, op, right):
         self.left = left
@@ -57,6 +61,7 @@ class BinOp(ASTNode):
     def __repr__(self):
         return f'({self.left}, {self.op}, {self.right})'
 
+# Class to represent numbers in AST
 class Num(ASTNode):
     def __init__(self, token):
         self.value = token.value
@@ -64,16 +69,16 @@ class Num(ASTNode):
     def __repr__(self):
         return f'ME_INT:{self.value}'
 
-# Parser class that tokenizes input
+# Parser is responsible for tokenizing input and parsing it into an AST
 class Parser:
     def __init__(self, text, fn="<stdin>"):
         self.text = text
         self.pos = Position(-1, 1, 0, fn, text)
         self.current_char = None
-        self.tokens = self.make_tokens()
+        self.tokens = self.make_tokens()  
         self.current_token = None
-        self.token_idx = -1
-        self.advance()
+        self.token_idx = -1  
+        self.advance()  
 
     def advance(self):
         self.token_idx += 1
@@ -81,44 +86,48 @@ class Parser:
             self.current_token = self.tokens[self.token_idx]
         return self.current_token
 
+    # Begin parsing the input
     def parse(self):
         return self.expression()
 
+    # Tokenize the input string into a list of tokens
     def make_tokens(self):
         tokens = []
-        self.advance_char()
+        self.advance_char()  
         while self.current_char is not None:
             if self.current_char in ' \t':
-                self.advance()  # Skip spaces and tabs
+                self.advance_char()  # Skip spaces and tabs
             elif self.current_char in DIGITS:
                 tokens.append(self.make_digit())
             elif self.current_char == '+':
                 tokens.append(Token(ME_PLUS))
-                self.advance()
+                self.advance_char()
             elif self.current_char == '-':
                 tokens.append(Token(ME_MINUS))
-                self.advance()
+                self.advance_char()
             elif self.current_char == '*':
                 tokens.append(Token(ME_MULTIPLY))
-                self.advance()
+                self.advance_char()
             elif self.current_char == '/':
                 tokens.append(Token(ME_DIVIDE))
-                self.advance()
+                self.advance_char()
             elif self.current_char == '(':
                 tokens.append(Token(ME_LPAREN))
-                self.advance()
+                self.advance_char()
             elif self.current_char == ')':
                 tokens.append(Token(ME_RPAREN))
-                self.advance()
+                self.advance_char()
             else:
                 tokens.append(self.make_illegal())
                 self.advance_char()
         return tokens
 
+    # Move to the next character 
     def advance_char(self):
         self.pos.advance(self.current_char)
         self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
 
+    # Create a token for a number by reading digits from the input
     def make_digit(self):
         num_str = ''
         while self.current_char is not None and self.current_char in DIGITS:
@@ -126,14 +135,23 @@ class Parser:
             self.advance_char()
         return Token(ME_INT, int(num_str))
 
+    # Handle illegal characters
+    def make_illegal(self):
+        pos_copy = self.pos.copy()
+        illegal_char = self.current_char
+        self.advance_char()
+        print(f"Illegal Character: '{illegal_char}'")
+        print(f"File {self.pos.fn}, line {pos_copy.line_num}, column {pos_copy.column_num}")
+        return Token(ME_ILLEGAL, illegal_char)
+
+    # Parse a factor - int or float
     def factor(self):
         token = self.current_token
-        if token.type == ME_INT:
+        if token.type in (ME_INT, ME_FLOAT): 
             self.advance()
             return Num(token)
-        else:
-            raise Exception(f"Expected number, but got {token.type}")
 
+    # Parse a factor - mul or div
     def term(self):
         left = self.factor()
         while self.current_token.type in (ME_MULTIPLY, ME_DIVIDE):
@@ -143,6 +161,7 @@ class Parser:
             left = BinOp(left, op_token, right)
         return left
 
+      # Parse a factor - plus or minus
     def expression(self):
         left = self.term()
         while self.current_token.type in (ME_PLUS, ME_MINUS):
@@ -157,6 +176,13 @@ def run(text):
     AST = parser.parse()
     return AST
 
+'''
+Output:
+M@E > 1+2*3
+(ME_INT:1, RR_ME_PLUS, (ME_INT:2, RR_ME_MULTIPLY, ME_INT:3))
+M@E > 1+4*5+1*3
+((ME_INT:1, RR_ME_PLUS, (ME_INT:4, RR_ME_MULTIPLY, ME_INT:5)), RR_ME_PLUS, (ME_INT:1, RR_ME_MULTIPLY, ME_INT:3))
+'''
 
 
 
